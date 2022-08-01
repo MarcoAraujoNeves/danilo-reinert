9d2a84a63e2e9874cc34cfdda895e92fabf41705
Marco Araujo <marcoaraujoneves@gmail.com>
2022-08-01 18:57:50 -0300

    Initial version of blog posts parsing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,46 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        // eslint-disable-next-line n/no-path-concat
+        path: `${__dirname}/posts`,
+        name: 'blog',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 730,
+            },
+          },
+          'gatsby-remark-autolink-headers',
+          'gatsby-plugin-catch-links',
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaults: {
+          formats: ['auto', 'webp'],
+          placeholder: 'blurred',
+          quality: 95,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: 'transparent',
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+      },
+    },
+  ],
 }
