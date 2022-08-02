526bd99363421371151f718097e9df3ab572f9c9
Marco Araujo <marcoaraujoneves@gmail.com>
2022-08-02 19:19:29 -0300

    Improves plugins config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,18 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaults: {
+          formats: ['auto', 'webp'],
+          placeholder: 'blurred',
+          quality: 95,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: 'transparent',
+        },
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: path.join(__dirname, 'src', 'assets', 'images'),
@@ -26,6 +38,7 @@ module.exports = {
             resolve: 'gatsby-remark-highlight-code',
             options: {
               terminal: 'none',
+              lineNumbers: true,
             },
           },
           {
@@ -39,23 +52,5 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-sharp',
-    //   options: {
-    //     defaults: {
-    //       formats: ['auto', 'webp'],
-    //       placeholder: 'blurred',
-    //       quality: 95,
-    //       breakpoints: [750, 1080, 1366, 1920],
-    //       backgroundColor: 'transparent',
-    //       tracedSVGOptions: {},
-    //       blurredOptions: {},
-    //       jpgOptions: {},
-    //       pngOptions: {},
-    //       webpOptions: {},
-    //       avifOptions: {},
-    //     },
-    //   },
-    // },
   ],
 }
