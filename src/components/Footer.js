b03a81982025dba4eb184e4f63982d20d7356386
Marco Araujo <marcoaraujoneves@gmail.com>
2022-08-06 13:08:31 -0300

    Turn footer badges into links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import FacebookBoxFillIcon from 'remixicon-react/FacebookBoxFillIcon'
 import GithubFillIcon from 'remixicon-react/GithubFillIcon'
 import TwitterFillIcon from 'remixicon-react/TwitterFillIcon'
@@ -9,6 +9,7 @@ import {
   footer,
   copyrights,
   column,
+  socialLink,
   columnsWrapper,
   badge,
 } from '../assets/css/footer.module.css'
@@ -47,6 +48,10 @@ const Footer = () => {
       return acc
     }, [])
 
+  const getPath = (type, value) => {
+    return `/${type}/${value.toLowerCase().replace(/ /g, '-')}`
+  }
+
   return (
     <footer className={footer}>
       <div className={columnsWrapper}>
@@ -54,36 +59,38 @@ const Footer = () => {
           <h3>Categories</h3>
 
           {categories.map(category => (
-            <span key={category} className={badge}>
-              {category}
-            </span>
+            <Link key={category} to={getPath('category', category)}>
+              <span className={badge}>{category}</span>
+            </Link>
           ))}
         </div>
         <div className={column}>
           <h3>Tags</h3>
 
           {tags.map(tag => (
-            <span key={tag} className={badge}>
-              {tag}
-            </span>
+            <Link key={tag} to={getPath('tag', tag)}>
+              <span key={tag} className={badge}>
+                {tag}
+              </span>
+            </Link>
           ))}
         </div>
         <div className={column}>
           <h3>Social Media</h3>
 
-          <a href="#">
+          <a href="#" className={socialLink}>
             <GithubFillIcon color="#171515" size={16} />
             Github
           </a>
-          <a href="#">
+          <a href="#" className={socialLink}>
             <LinkedinBoxFillIcon color="#0072b1" size={16} />
             LinkedIn
           </a>
-          <a href="#">
+          <a href="#" className={socialLink}>
             <TwitterFillIcon color="#1DA1F2" size={16} />
             Twitter
           </a>
-          <a href="#">
+          <a href="#" className={socialLink}>
             <FacebookBoxFillIcon color="#4267b2" size={16} />
             Facebook
           </a>
