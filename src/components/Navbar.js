1dad91c87257777fd43757e04b7dabb10becac11
Marco Araujo <marcoaraujoneves@gmail.com>
2022-08-11 21:05:30 -0300

    Improves and adds mobile state to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,70 @@ import {
   header,
   searchWrapper,
   toggleThemeButton,
+  menuToggle,
+  openMenu,
 } from '../assets/css/navbar.module.css'
+
+const NavLinks = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/" className="nav-link" activeClassName="active-link">
+          HOME
+        </Link>
+      </li>
+      <li>
+        <Link to="/about/" className="nav-link" activeClassName="active-link">
+          ABOUT
+        </Link>
+      </li>
+      <li>
+        <Link to="/posts/" className="nav-link" activeClassName="active-link">
+          POSTS
+        </Link>
+      </li>
+      <li>
+        <Link to="/contact/" className="nav-link" activeClassName="active-link">
+          CONTACT
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+const SearchInput = ({ search, setSearch, handleSearch }) => (
+  <form className={searchWrapper} onSubmit={handleSearch}>
+    <input
+      type="text"
+      placeholder="Search"
+      value={search}
+      onChange={e => setSearch(e.target.value)}
+    />
+
+    <button type="submit">
+      <SearchLineIcon color="#ffffff" size={14} />
+    </button>
+  </form>
+)
+
+const ThemeToggle = ({ colorMode, setColorMode }) => (
+  <>
+    {colorMode ? (
+      <button
+        className={toggleThemeButton}
+        onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+      >
+        {colorMode === 'light' ? (
+          <SunFillIcon color="#35475e" size={18} />
+        ) : (
+          <MoonClearFillIcon color="#a6bbcd" size={18} />
+        )}
+      </button>
+    ) : (
+      false
+    )}
+  </>
+)
 
 const Navbar = ({ location }) => {
   const params = location && new URLSearchParams(location.search)
@@ -19,6 +82,7 @@ const Navbar = ({ location }) => {
     params && params.get('search') && params.get('search').toLowerCase()
 
   const [search, setSearch] = useState(searchValue || '')
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const { colorMode, setColorMode } = useContext(ThemeContext)
 
   const handleSearch = event => {
@@ -28,71 +92,22 @@ const Navbar = ({ location }) => {
   }
 
   return (
-    <header className={header}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" className="nav-link" activeClassName="active-link">
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about/"
-              className="nav-link"
-              activeClassName="active-link"
-            >
-              ABOUT
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/posts/"
-              className="nav-link"
-              activeClassName="active-link"
-            >
-              POSTS
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact/"
-              className="nav-link"
-              activeClassName="active-link"
-            >
-              CONTACT
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <header className={`${header} ${isMenuOpen ? openMenu : ''}`}>
+      <NavLinks />
 
-      {colorMode ? (
-        <button
-          className={toggleThemeButton}
-          onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
-        >
-          {colorMode === 'light' ? (
-            <SunFillIcon color="#35475e" size={18} />
-          ) : (
-            <MoonClearFillIcon color="#a6bbcd" size={18} />
-          )}
-        </button>
-      ) : (
-        false
-      )}
+      <ThemeToggle colorMode={colorMode} setColorMode={setColorMode} />
 
-      <form className={searchWrapper} onSubmit={handleSearch}>
-        <input
-          type="text"
-          placeholder="Search"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
+      <SearchInput
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
 
-        <button type="submit">
-          <SearchLineIcon color="#ffffff" size={14} />
-        </button>
-      </form>
+      <div className={menuToggle} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </header>
   )
 }
